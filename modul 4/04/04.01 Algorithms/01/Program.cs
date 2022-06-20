c33465aa4c77b21129e0a20214bd42cf5a9bfd12
Klameta <ivanavalcheva@gmail.com>
2022-06-20 23:10:14 +0300

    Adding the code from the last couple days

---
+++ modul 4/04/04.01 Algorithms/01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            SelectionSort(nums);
+            Console.WriteLine(string.Join(" ", nums));
+        }
+        static void SelectionSort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                int minNums = nums[i];
+                int index = i;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] < minNums)
+                    {
+                        minNums = nums[j];
+                        index = j;
+                    }
+                }
+
+                int swapped = nums[i];
+                nums[i] = nums[index];
+                nums[index] = swapped;
+            }
+        }
+
+        private static int Less(int[] nums, int i, int currentNum)
+        {
+            if (currentNum < i)
+            {
+                int temp = nums[i];
+                nums[i] = currentNum;
+                currentNum = temp;
+            }
+
+            return currentNum;
+        }
+    }
+}
---
+++ modul 4/04/04.01 Algorithms/05 Running/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+namespace _05_Running
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstDay = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondDay = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] result = new int[firstDay.Length + secondDay.Length];
+            Combining(result, firstDay, secondDay);
+            SelectionSort(result);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        static void Combining(int[] result, int[] firstDay, int[] secondDay)
+        {
+            int counter = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i < firstDay.Length)
+                {
+                    result[i] = firstDay[i];
+                }
+                else
+                {
+                    result[i] = secondDay[counter];
+                    counter++;
+                }
+            }
+        }
+        static void SelectionSort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                int maxNum = nums[i];
+                int index = i;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] < maxNum)
+                    {
+                        maxNum = nums[j];
+                        index = j;
+                    }
+                }
+                Swapped(nums, i, index);
+            }
+
+            static void Swapped(int[] nums, int i, int index)
+            {
+                int swapped = nums[i];
+                nums[i] = nums[index];
+                nums[index] = swapped;
+            }
+        }
+    }
+}
