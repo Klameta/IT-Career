58391df55f065e812b4bfdd01d85830426f7dbc6
Klameta <74143760+Klameta@users.noreply.github.com>
2022-07-03 11:12:11 +0300

    Done with all of my exams this month wooo

---
+++ modul 4/Exam 4 Modul/01/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Book
+    {
+        private string title;
+
+        private double rating;
+
+        public Book(string title, double rating)
+        {
+            this.title = title;
+            this.rating = rating;
+            //TODO: Добавете вашия код тук …
+
+        }
+        public string Title
+        {
+        get { return title; }
+        set { title = value; }
+            //TODO: Добавете вашия код тук …
+
+        }
+
+        public double Rating
+
+        {
+        get { return rating; }
+        set { rating = value; }
+    }
+
+        public override string ToString()
+
+        {
+            return $"Book {this.title} is with rating {this.rating} rating.";
+            //TODO: Добавете вашия код тук …
+
+        }
+    }
---
+++ modul 4/Exam 4 Modul/01/BookLibrary.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BookLibrary
+{
+    private string name;
+
+    private List<Book> books = new List<Book>();
+
+    public BookLibrary(string name)
+    {
+        this.name = name;
+        //TODO: Добавете вашия код тук …
+
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+        //TODO: Добавете вашия код тук …
+
+    }
+
+    public List<Book> Books
+    {
+        get { return books; }
+        set { books = value; }
+        //TODO: Добавете вашия код тук …
+
+    }
+
+    public void AddBook(string title, double rating)
+    {
+        Book book = new Book(title, rating);
+        this.books.Add(book);
+        //TODO: Добавете вашия код тук …
+
+    }
+
+    public double AverageRating()
+
+    {
+        int count = 0;
+        double sum = 0;
+        foreach (var book in books)
+        {
+            sum += book.Rating;
+            count++;
+        }
+        return (double)sum / count;
+        //TODO: Добавете вашия код тук …
+
+    }
+
+    public List<string> GetBooksByRating(double rating)
+    {
+        List<Book> tempBooks = Books;
+        tempBooks =tempBooks.Where(x => x.Rating > rating).ToList();
+        List<string> bookTitles = new List<string>();
+        foreach (var book in tempBooks)
+        {
+            bookTitles.Add(book.Title);
+        }
+        return bookTitles;
+        //TODO: Добавете вашия код тук …
+
+    }
+
+    public List<Book> SortByTitle()
+
+    {
+        books = books.OrderBy(x => x.Title).ToList();
+        return books;
+        //TODO: Добавете вашия код тук …
+
+    }
+
+    public List<Book> SortByRating()
+
+    {
+        books = books.OrderByDescending(x => x.Rating).ToList();
+        return books;
+        //TODO: Добавете вашия код тук …
+
+    }
+
+    public string[] ProvideInformationAboutAllBooks()
+
+    {
+        string[] printInfo = new string[books.Count];
+        for (int i = 0; i < books.Count; i++)
+        {
+            printInfo[i] = books[i].ToString();
+        }
+        return printInfo;
+
+        //TODO: Добавете вашия код тук …
+
+    }
+
+    public bool CheckBookIsInBookLibrary(string title)
+
+    {
+        foreach (var book in books)
+        {
+            if (book.Title == title)
+            {
+                return true;
+            }
+        }
+        return false;
+        //TODO: Добавете вашия код тук …
+
+    }
+}
