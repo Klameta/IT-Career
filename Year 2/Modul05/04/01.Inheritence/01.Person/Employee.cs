b12c805c1343884fd9276ec44ae78e95d18d89ee
Klameta <ivanavalcheva@gmail.com>
2022-09-18 14:40:19 +0300

    Inheritence

---
+++ Year 2/Modul05/04/01.Inheritence/01.Person/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Person
+{
+    class Employee : Person
+    {
+        private string company;
+
+        public string Company
+        {
+            get { return company; }
+            set { company = value; }
+        }
+
+        public Employee(int age, string name, string company) : base(age, name)
+        {
+            Company = company;
+        }
+    }
+}
---
+++ Year 2/Modul05/04/01.Inheritence/03.HierarchyInheratence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.HierarchyInheratence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+            cat.Meow();
+            cat.Eat();
+            dog.Eat();
+            dog.Bark();
+
+        }
+    }
+}
