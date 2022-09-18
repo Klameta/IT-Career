8d7bdd4147a7e780be9a4711ebac114b91773b4a
Klameta <ivanavalcheva@gmail.com>
2022-09-18 16:28:55 +0300

    Inheritence 02 done

---
+++ Year 2/Modul05/04/02.ReusingClasses/05.StackOfStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.StackOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+
+            stackOfStrings.Push("asd");
+            stackOfStrings.Push("gf");
+            stackOfStrings.Push("kjsf");
+            stackOfStrings.Push("sdgkj");
+
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.Peek());
+            Console.WriteLine(stackOfStrings.IsEmpty());
+        }
+    }
+}
