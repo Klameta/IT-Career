4b0912d02bd5e020414924fd7b4f56b8f5076c94
Klameta <ivanavalcheva@gmail.com>
2022-09-22 12:08:18 +0300

    06 DONE

---
+++ Year 2/Modul05/04/06.ExAbstraction/01.Workers/ContractedEmployee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Workers
+{
+    class ContractedEmployee : BaseEmployee
+    {
+        private string employeeTask;
+        private string employeeDepartment;
+
+        public string EmployeeTask
+        {
+            get { return employeeTask; }
+            set { employeeTask = value; }
+        }
+        public string EmployeeDepartment
+        {
+            get { return employeeDepartment; }
+            set { employeeDepartment = value; }
+        }
+
+
+        public ContractedEmployee(string employeeID, string employeeName
+            , string employeeAddress, string employeeTask, string employeeDepartment)
+            : base(employeeID, employeeName, employeeAddress)
+        {
+            EmployeeTask = employeeTask;
+            EmployeeDepartment = employeeDepartment;
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine(EmployeeTask);
+        }
+
+        public override double CalculateSalary(int workingHours)
+        {
+            return 250 + workingHours * 20;
+        }
+
+        public override string GetDepartment()
+        {
+            return EmployeeDepartment;
+        }
+    }
+}
---
+++ Year 2/Modul05/04/06.ExAbstraction/01.Workers/FullTimeEmployee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Workers
+{
+    class FullTimeEmployee : BaseEmployee
+    {
+        private string employeePosition;
+        private string employeeDepartment;
+
+        public string EmployeePosition
+        {
+            get { return employeePosition; }
+            private set { employeePosition = value; }
+        }
+        public string EmployeeDepartment
+        {
+            get { return employeeDepartment; }
+            set { employeeDepartment = value; }
+        }
+
+        public FullTimeEmployee(string employeeID, string employeeName,
+            string employeeAddress, string employeePosition, string employeeDepartment)
+            : base(employeeID, employeeName, employeeAddress)
+        {
+            EmployeePosition = employeePosition;
+            EmployeeDepartment = employeeDepartment;
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine(EmployeePosition);
+            Console.WriteLine(EmployeeDepartment);
+        }
+        public override double CalculateSalary(int workingHours)
+        {
+            return 250 + workingHours * 10.80;
+        }
+
+        public override string GetDepartment()
+        {
+            return EmployeeDepartment;
+        }
+
+    }
+}
