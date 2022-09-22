1b1600e5ca1a8eabfe0b2ff12e4b97249fd9a962
Klameta <ivanavalcheva@gmail.com>
2022-09-22 10:35:24 +0300

    04 doc DONE

---
+++ Year 2/Modul05/04/04.04.Interfaces/01.Figures/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(10);
+            circle.Draw();
+
+            Rectangle rectangle = new Rectangle(10, 5);
+            rectangle.Draw();
+        }
+    }
+}
