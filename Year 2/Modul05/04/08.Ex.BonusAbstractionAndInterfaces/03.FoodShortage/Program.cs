ed3240163f7c307cddacf8e3be272e230ac6204d
Klameta <ivanavalcheva@gmail.com>
2022-09-23 09:51:43 +0300

    08 DONE

--- Year 2/Modul05/04/08.Ex.BonusAbstractionAndInterfaces/03.FoodShortage/Program.cs
+++ Year 2/Modul05/04/08.Ex.BonusAbstractionAndInterfaces/03.FoodShortage/Program.cs
@@ -14,28 +14,36 @@ namespace _03.FoodShortage
             int people = int.Parse(Console.ReadLine());
 
             string[] cmdArgs = Console.ReadLine().Split();
-            while(cmdArgs[0]!="End")
+            for (int i = 0; i < people; i++)
             {
-                if (cmdArgs.Length == 4)
+                if (!hungries.ContainsKey(cmdArgs[0]))
                 {
-                    if (!hungries.ContainsKey(cmdArgs[0]))
+                    if (cmdArgs.Length == 4)
                     {
                         Human human = new Human(cmdArgs[0], int.Parse(cmdArgs[1]), cmdArgs[2], cmdArgs[3]);
                         hungries.Add(cmdArgs[0], human);
                     }
                     else
                     {
+                        Rebel rebel = new Rebel(cmdArgs[0], int.Parse(cmdArgs[1]), cmdArgs[2]);
+                        hungries.Add(cmdArgs[0], rebel);
+                    }
+                }
+                cmdArgs = Console.ReadLine().Split();
+            }
+
+            while (cmdArgs[0] != "End")
+            {
+                if (cmdArgs.Length == 4)
+                {
+                    if (hungries.ContainsKey(cmdArgs[0]))
+                    {
                         hungries[cmdArgs[0]].BuyFood();
                     }
                 }
                 else
                 {
-                    if (!hungries.ContainsKey(cmdArgs[0]))
-                    {
-                        Rebel rebel = new Rebel(cmdArgs[0], int.Parse(cmdArgs[1]), cmdArgs[2]);
-                        hungries.Add(cmdArgs[0], rebel);
-                    }
-                    else
+                    if (hungries.ContainsKey(cmdArgs[0]))
                     {
                         hungries[cmdArgs[0]].BuyFood();
                     }
