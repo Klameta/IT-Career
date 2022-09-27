7d930e62178d70d4e3f92ff7c54184080d04ab04
Klameta <ivanavalcheva@gmail.com>
2022-09-27 22:17:22 +0300

    01 done 03 DONE

---
+++ Year 2/Modul05/05.Polymorphism/03.AbstractClasses/01.Shapes/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Shapes
+{
+    class Circle : Shape
+    {
+        private double radius;
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Math.PI * 2 * Radius;
+        }
+
+        public override string Draw()
+        {
+            return "Imagine a circle...";
+        }
+    }
+}
---
+++ Year 2/Modul05/05.Polymorphism/03.AbstractClasses/01.Shapes/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Shapes
+{
+    public abstract class Shape
+    {
+        public abstract double CalculatePerimeter();
+        public abstract double CalculateArea();
+
+        public virtual string Draw()
+        {
+            return string.Empty;
+        }
+    }
+}
