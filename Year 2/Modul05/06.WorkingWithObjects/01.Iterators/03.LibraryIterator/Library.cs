3215338c2c66fe3e28fe1ddb5ed9999973ce0c3e
Klameta <ivanavalcheva@gmail.com>
2022-09-30 21:30:20 +0300

    03, 04 done

---
+++ Year 2/Modul05/06.WorkingWithObjects/01.Iterators/03.LibraryIterator/Library.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.LibraryIterator
+{
+    internal class Library : IEnumerable<Book>
+    {
+        private List<Book> books { get; set; }
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var book in books)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ Year 2/Modul05/06.WorkingWithObjects/01.Iterators/04.ListIterator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ListIterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            ListIterator<string> listIterator = new ListIterator<string>();
+            while (cmdArgs[0] != "END")
+            {
+                switch (cmdArgs[0])
+                {
+                    case "Create":
+                        listIterator = new ListIterator<string>(cmdArgs
+                            .Skip(1)
+                            .Take(cmdArgs.Length - 1)
+                            .ToArray());
+                        break;
+                    case "Move":
+                        Console.WriteLine(listIterator.Move());
+                        break;
+                    case "Print":
+                        listIterator.Print();
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listIterator.HasNext());
+                        break;
+                    default:
+                        break;
+                }
+                cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
