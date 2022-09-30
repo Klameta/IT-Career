48c892d1acfd0224a05fa98db92930052c83c4dd
Klameta <ivanavalcheva@gmail.com>
2022-09-30 20:50:42 +0300

    01, 02 done

---
+++ Year 2/Modul05/06.WorkingWithObjects/01.Iterators/02.Library/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.Library
+{
+    internal class Book
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+
+        public string Title { get;private set; }
+        public int Year { get;private set; }
+        public IReadOnlyList<string> Authors { get;private set; }
+
+    }
+}
