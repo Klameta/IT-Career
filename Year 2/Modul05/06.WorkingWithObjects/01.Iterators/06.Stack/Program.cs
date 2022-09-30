1da8e3485fa417d7cf29c66876847d8489754781
Klameta <ivanavalcheva@gmail.com>
2022-09-30 22:33:35 +0300

    06 done

---
+++ Year 2/Modul05/06.WorkingWithObjects/01.Iterators/06.Stack/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            string[] cmdArgs = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            while (cmdArgs[0] != "END")
+            {
+                switch (cmdArgs[0])
+                {
+                    case "Push":
+                        stack.Push(cmdArgs
+                            .Skip(1)
+                            .Take(cmdArgs.Length - 1)
+                            .ToArray());
+                        break;
+                    case "Pop":
+                        stack.Pop();
+                        break;
+                }
+                cmdArgs = Console.ReadLine()
+                .Split(',', ' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+            foreach (var value in stack)
+            {
+                Console.WriteLine(value);
+            }
+            foreach (var value in stack)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
