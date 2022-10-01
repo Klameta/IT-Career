24f121a46c6ecd3e172f54d9cb7cb7d50d8f7252
Klameta <ivanavalcheva@gmail.com>
2022-10-01 22:38:01 +0300

    07 done 02 DONE

---
+++ Year 2/Modul05/06.WorkingWithObjects/02.Comperer/07.LogicForEquality/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.LogicForEquality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            PersonComperer personComperer = new PersonComperer();
+            SortedSet<Person> peopleSet = new SortedSet<Person>(personComperer);
+            HashSet<Person> peopleHash = new HashSet<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(cmdArgs[0], int.Parse(cmdArgs[1]));
+
+                peopleSet.Add(person);
+                peopleHash.Add(person);
+            }
+
+            Console.WriteLine(peopleSet.Count);
+            Console.WriteLine(peopleHash.Count);
+        }
+    }
+}
