1af7ea23ea6faf186c02dff07f8e88ebdc20a911
Klameta <ivanavalcheva@gmail.com>
2022-10-02 01:50:56 +0300

    01.08 started 03.00 done

---
+++ Year 2/Modul05/06.WorkingWithObjects/01.Iterators/08.VetenarianClinic/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.VetenarianClinic
+{
+    internal class Animal
+    {
+        public string Name { get; private set; }
+        public int Age { get; set; }
+        public string Type { get; private set; }
+
+        public Animal(string name, int age, string type)
+        {
+            Name = name;
+            Age = age;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age} {Type}";
+        }
+    }
+}
---
+++ Year 2/Modul05/06.WorkingWithObjects/01.Iterators/08.VetenarianClinic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08.VetenarianClinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
