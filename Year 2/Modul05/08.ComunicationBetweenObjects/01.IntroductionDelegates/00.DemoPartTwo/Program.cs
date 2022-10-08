142074b53df56f2809ff1004abd5116fd4f58c16
Klameta <ivanavalcheva@gmail.com>
2022-10-08 19:13:04 +0300

    00.00, 00.01 done

---
+++ Year 2/Modul05/08.ComunicationBetweenObjects/01.IntroductionDelegates/00.DemoPartTwo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _00.DemoPartTwo
+{
+    internal class Program
+    {
+        delegate int myDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            myDelegate ab = (a, b) => a + b;
+            int result = ab(1, 1);
+            Console.WriteLine(result);
+
+            myDelegate s = sum;
+            Console.WriteLine();
+
+            myDelegate d = new myDelegate(sum);
+            Console.WriteLine(d(4,5));
+
+            myDelegate ab1 = (a,b) => a - b;
+            Console.WriteLine(ab1(3,2));
+
+        }
+
+        static int sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
