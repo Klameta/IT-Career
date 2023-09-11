2b19d6b8b971ee1683b391d04d3c6f58b8c9d80f
Klameta <ivanavalcheva@gmail.com>
2023-09-11 08:31:10 +0300

    01 done

---
+++ Year 3/02. Recursion/01. Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01._Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fiboncci(input));
+        }
+
+        public static int Fiboncci(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            return Fiboncci(n - 1) + Fiboncci(n - 2);
+        }
+    }
+}
