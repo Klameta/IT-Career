dc64f941da6a3915adc89bd3a95a3f6d36526674
Klameta <ivanavalcheva@gmail.com>
2023-09-11 05:27:53 +0300

    01, 02 done

---
+++ Year 3/01.ProjectingSums/01. Greedy Algorithms/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01.ProjectingSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int finalSum = 18;
+            int[] coins = new int[] { 10, 10, 5, 5, 2, 2, 1, 1 };
+
+            if (!ProjectingSums(finalSum, coins))
+            {
+                Console.WriteLine("Sum hs not been found");
+            };
+        }
+
+        private static bool ProjectingSums(int finalSum, int[] coins)
+        {
+            int currSum = 0;
+            Queue<int> resultCoins = new Queue<int>();
+            for (int i = 0; i < coins.Length; i++)
+            {
+                int currCoin = coins[i];
+                if (currSum + currCoin > finalSum) continue;
+
+                currSum += currCoin;
+                resultCoins.Enqueue(currCoin);
+
+                if (currSum == finalSum)
+                {
+                    Console.WriteLine("Sum has been found");
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Year 3/01.ProjectingSums/02.EgyptianFractions/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.EgyptianFractions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = int.Parse(Console.ReadLine());
+            int q = int.Parse(Console.ReadLine());
+            List<int> denode = new List<int>();
+
+            Console.Write($"{p}/{q} = 1/");
+            while (p > 1)
+            {
+                int r = (p + q) / p;
+                denode.Add(r);
+
+                p = p * r - q;
+                q *= r;
+
+                int nod = NOD(p, q);
+
+                p /= nod;
+                q /=  nod;
+            }
+            denode.Add(q);
+
+            Console.WriteLine(string.Join(" + 1/", denode));
+        }
+
+        private static int NOD(int p, int q)
+        {
+            while (q != 0)
+            {
+                int r = p % q;
+                p = q;
+                q = r;
+            }
+            return p;
+        }
+    }
+}
