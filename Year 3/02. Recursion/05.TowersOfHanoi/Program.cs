4cf372065cf6e7cd33496d118b497267dbc2b8a1
Klameta <ivanavalcheva@gmail.com>
2023-09-13 17:15:39 +0300

    05 done

---
+++ Year 3/02. Recursion/05.TowersOfHanoi/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.TowersOfHanoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 1; int b = 2; int c = 3;
+
+            Hanoi(n, a, b, c);
+        }
+
+        private static void Hanoi(int n, int a, int b, int c)
+        {
+            if (n == 0) return;
+
+            Hanoi(n-1, a, b, c);
+            Console.WriteLine($"{a} -> {c}");
+            Hanoi(n, a, b, c);
+        }
+    }
+}
