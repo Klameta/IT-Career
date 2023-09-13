62a1f5f154f22f19be0c42b7cb0d05ffed3242ae
Klameta <ivanavalcheva@gmail.com>
2023-09-13 16:42:39 +0300

    03 04 done

---
+++ Year 3/02. Recursion/04.Labyrinth/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _04.Labyrinth
+{
+    internal class Program
+    {
+        static int rows;
+        static int cols;
+        static char[,] labyrinth;
+        List<char> path = new List<char>();
+
+        static void Main(string[] args)
+        {
+            ReadLab();
+        }
+
+        private static void ReadLab()
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            labyrinth = new char[rows, cols];
+            cols = input[1];
+            rows = input[0];
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] currRow = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    labyrinth[row, col] = currRow[col];
+                }
+            }
+        }
+    }
+}
