8988ec93359bda4eeef18d7a7f0804b1869b4f04
Klameta <ivanavalcheva@gmail.com>
2023-09-13 17:57:19 +0300

    01 02 done

--- Year 3/01.Combinatorics/01.Permutations/Program.cs
+++ Year 3/01.Combinatorics/01.Permutations/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        static int n;
+        static int[] permutations;
+        static int[] elements;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int index = int.Parse(Console.ReadLine());
+
+            Generte(index);
+        }
+
+        private static void Generte(int index)
+        {
+            if (index == 0)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                    return;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!used = true)
+                {
+                    used[i] = true;
+                    permutations[index] = ;
+
+                }
+            }
         }
     }
 }
---
+++ Year 3/01.Combinatorics/02.Combinations/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.Combinations
+{
+    internal class Program
+    {
+        static int n, k;
+        static int[] elements;
+        static int[] combinations;
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new int[k];
+
+            Generate(0, 1);
+        }
+
+        private static void Generate(int index, int start)
+        {
+            if (index == k)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = start; i < n; i++)
+            {
+                combinations[index] = i;    
+                Generate(index + 1, i + 1);
+            }
+        }
+    }
+}
