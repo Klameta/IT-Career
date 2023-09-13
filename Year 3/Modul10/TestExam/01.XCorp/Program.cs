dd193fcc5d5f548bcc86e307a4d1443aade2a502
Klameta <ivanavalcheva@gmail.com>
2023-09-13 18:51:28 +0300

    test exam done

---
+++ Year 3/Modul10/TestExam/01.XCorp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.XCorp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vaucherCnt = 0;
+
+            int inputDays = int.Parse(Console.ReadLine());
+            int daysMoney = inputDays * 20;
+
+            while (daysMoney != 0)
+            {
+                if (daysMoney >= 100) daysMoney -= 100;
+                else daysMoney -= 20;
+                vaucherCnt++;
+            }
+
+            Console.WriteLine(vaucherCnt);
+        }
+    }
+}
---
+++ Year 3/Modul10/TestExam/02.MutiplyingPosNums/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.MutiplyingPosNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int m = nums[0];
+            int n = nums[1];
+
+            Console.WriteLine(mult(m, n));
+        }
+        static int mult(int m, int n)
+        {
+            if (n == 1)
+            {
+                return m;
+            }
+            else
+            {
+                return mult(m, n - 1) + m;
+            }
+        }
+
+    }
+}
