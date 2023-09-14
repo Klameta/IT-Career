1794f4c0cdb056b662cc393ddb7e6830103adf16
Klameta <ivanavalcheva@gmail.com>
2023-09-14 16:43:53 +0300

    done

---
+++ Year 3/Modul10/01.Combinatorics/06.Sums/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06.Sums
+{
+    internal class Program
+    {
+        static int n;
+        static int[] nums;
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            nums = new int[n];
+
+            BreakingSum(n,0);
+        }
+
+        private static void BreakingSum(int n, int index)
+        {
+            if (n==0)
+            {
+                Console.WriteLine(string.Join("+", nums.Take(index)));
+            };
+        }
+    }
+}
