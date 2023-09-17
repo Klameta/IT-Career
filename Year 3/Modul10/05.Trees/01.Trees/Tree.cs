c36ea8aea7cfce6ae4dac117508dfaf9fdedd2a9
Klameta <ivanavalcheva@gmail.com>
2023-09-17 15:09:10 +0300

    01 done

---
+++ Year 3/Modul10/05.Trees/01.Trees/Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Trees
+{
+    class Tree<T>
+    {
+        private T value;
+        private IList<Tree<T>> children;
+
+        public Tree(T value, params Tree<T>[] children)
+        {
+            this.value = value;
+            this.children = children;
+        }
+
+        public void Print(int indent = 0)
+        {
+            Console.Write(new string(' ', 2 * indent));
+            Console.WriteLine(value);
+
+            foreach (var child in children)
+            {
+                child.Print(indent + 1);
+            }
+        }
+    }
+}
